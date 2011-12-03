e62160e8fed1f12791c7e66167f1ed03f006e36e
qw3r <demeter.istvan@gmail.com>
2011-12-03 14:28:32 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ombrello::Application.config.secret_token = 'e01bfa3b0f57e9fbeca10d32f7410b7e8b77e90436db429a80c383f749990cf848e0dcb782a6204866d489a81426fc4a21575ac1d1021cc9334c11bba5eb2906'
